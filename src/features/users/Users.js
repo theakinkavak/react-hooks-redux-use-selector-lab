54fcde564012f4a298a0ae0ca15e39c6730121ef
Akin <akinkavak1@gmail.com>
2022-03-28 00:55:52 +0300

    Utilize Provider component and useSelector hook to access Redux store content

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,15 +1,23 @@
 import React from "react";
 // add any needed imports here
+import { useSelector } from "react-redux";
 
 function Users() {
+
+  const users = useSelector(state => state.users);
+  const userCount = useSelector(state => state.users.length);
+
   return (
-    <div>
+    <>
       <ul>
-        Users!
         {/* Write code here that displays the usernames of all users in the Redux store */}
         {/* In addition, display the total number of users curently in the store */}
+        {users.map(user => (
+          <li key={user.username}>{user.username}</li>
+        ))}
       </ul>
-    </div>
+      Total Users: {userCount}
+    </>
   );
 }
 
